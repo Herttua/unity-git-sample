73f6a3103d610ff52e59304997829eceb58e6bab
Juha Salinmäki <juha.salinmaki@gmail.com>
2018-06-27 11:41:29 +0300

    Improving the warrior movement

--- ExampleGameProject/Assets/Scripts/MoveWarrior.cs
+++ ExampleGameProject/Assets/Scripts/MoveWarrior.cs
@@ -6,6 +6,8 @@ public class MoveWarrior : MonoBehaviour
 {
     public float moveSpeed = 10.0f;
 
+    public int facing = 0; //0 = up, 1 = right, 2 = down, 3 = left
+
 	// Use this for initialization
 	void Start ()
     {
@@ -22,6 +24,56 @@ public class MoveWarrior : MonoBehaviour
         else if(Input.GetKey(KeyCode.DownArrow))
         {
             transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+        }
+
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            if(facing > 0 )
+            facing--;
+        }
+        else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            if(facing < 3)
+            {
+                facing++;
+            }
+        }
+
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            switch(facing)
+            {
+                case 0:
+                    facing = 3;
+                    break;
+
+                case 1:
+                    facing = 0;
+                    break;
+
+                case 2:
+                    facing = 3;
+                    break;
+
+                case 3:
+                    facing = 0;
+                    break;
+
+                default: break;
+            }
+        }
+
+        switch(facing)
+        {
+            case 0:
+                transform.Rotate(new Vector3(0, 0, 0));
+                break;
+
+            case 1:
+                transform.Rotate(new Vector3(90, 0, 0));
+                break;
+
+            default: break;
         }
 	}
 }
