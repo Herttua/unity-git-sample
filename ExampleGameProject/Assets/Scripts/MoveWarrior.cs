338ac5afb770ff1d05bf990d6ca7c06ff7c6b771
Juha Salinmäki <juha.salinmaki@gmail.com>
2018-06-27 11:19:45 +0300

    Added script for moving warrior. (Not working yet)

---
+++ ExampleGameProject/Assets/Scripts/MoveWarrior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWarrior : MonoBehaviour
+{
+    public float moveSpeed = 10.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+	    	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+        else if(Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+	}
+}
